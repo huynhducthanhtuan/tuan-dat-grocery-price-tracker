ccfe5c803d755c2a561beeb3052ada2dbe1464c9
huynhducthanhtuan <tuan40655@gmail.com>
2026-08-08 16:56:58 +0700

    Chore: Add @ts-ignore for CSS module style.css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import initialItems from "../data/items.json";
 import { getItemImage, fallbackImages } from "./helpers/itemImages";
 
